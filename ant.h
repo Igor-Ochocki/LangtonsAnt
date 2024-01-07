3221a9e0a4f63709d9c8062157b0a59dda628af7
Igor Ochocki <igorochocki@zielone.eu>
2024-01-07 17:25:48 +0100

    Add board loading from file

--- ant.h
+++ ant.h
@@ -4,27 +4,27 @@
 #define WHITE_COLOR 0
 #define ANT 2
 
-#define LINE_VERTICAL '┃'
-#define LINE_HORIZONTAL '━'
-#define LINE_DOWN_RIGHT '┏'
-#define LINE_DOWN_LEFT '┓'
-#define LINE_UP_RIGHT '┗'
-#define LINE_UP_LEFT '┛'
-#define SQUARE_WHITE ' '
-#define SQUARE_BLACK '█'
-#define ARROW_NORTH_WHITE '△'
-#define ARROW_NORTH_BLACK '▲'
-#define ARROW_EAST_WHITE '▷'
-#define ARROW_EAST_BLACK '▶'
-#define ARROW_SOUTH_WHITE '▽'
-#define ARROW_SOUTH_BLACK '▼'
-#define ARROW_WEST_WHITE '◁'
-#define ARROW_WEST_BLACK '◀'
-#define LINE_UP_RIGHT_DOWN '┣'
-#define LINE_UP_LEFT_RIGHT '┻'
-#define LINE_RIGHT_DOWN_LEFT '┳'
-#define LINE_UP_DOWN_LEFT '┫'
-#define LINE_UP_RIGHT_DOWN_LEFT '╋'
+#define LINE_VERTICAL "┃"
+#define LINE_HORIZONTAL "━"
+#define LINE_DOWN_RIGHT "┏"
+#define LINE_DOWN_LEFT "┓"
+#define LINE_UP_RIGHT "┗"
+#define LINE_UP_LEFT "┛"
+#define SQUARE_WHITE " "
+#define SQUARE_BLACK "█"
+#define ARROW_NORTH_WHITE "△"
+#define ARROW_NORTH_BLACK "▲"
+#define ARROW_EAST_WHITE "▷"
+#define ARROW_EAST_BLACK "▶"
+#define ARROW_SOUTH_WHITE "▽"
+#define ARROW_SOUTH_BLACK "▼"
+#define ARROW_WEST_WHITE "◁"
+#define ARROW_WEST_BLACK "◀"
+#define LINE_UP_RIGHT_DOWN "┣"
+#define LINE_UP_LEFT_RIGHT "┻"
+#define LINE_RIGHT_DOWN_LEFT "┳"
+#define LINE_UP_DOWN_LEFT "┃"
+#define LINE_UP_RIGHT_DOWN_LEFT "╋"
 
 enum Directions {
     UP,
@@ -34,7 +34,7 @@ enum Directions {
 };
 
 typedef struct {
-    Directions direction;
+    enum Directions direction;
     int col;
     int row;
     int color;
---
+++ load_board.c
@@ -0,0 +1,65 @@
+#include "load_board.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+int **loadBoardFromFile(char *fileName, int *rows_count, int *columns_count)
+{
+    setlocale(LC_ALL, "C.UTF-8");
+    if (fileName == NULL)
+    {
+        printf("Nie podano nazwy pliku do wczytania.\n");
+        exit(1);
+    }
+    FILE *in = fopen(fileName, "r");
+    if (in == NULL)
+    {
+        printf("%s: nie mogę czytać %s", __FILE__, fileName);
+        exit(1);
+    }
+    wchar_t symbol;
+    while ((symbol = fgetwc(in)) != WEOF)
+    {
+        if (symbol == '\n')
+        {
+            (*rows_count)++;
+            continue;
+        }
+        (*columns_count)++;
+    }
+    fclose(in);
+    (*rows_count)++;
+    (*columns_count) = (*columns_count) / (*rows_count);
+
+    (*columns_count) /= 2;
+    (*rows_count) /= 2;
+    int **board_structure = malloc(sizeof *board_structure * (*rows_count));
+    for (int i = 0; i < *rows_count; i++)
+    {
+        board_structure[i] = malloc(sizeof *board_structure[i] * (*columns_count));
+    }
+    in = fopen(fileName, "r");
+    wchar_t black_wall = L' ';
+    wchar_t white_wall = L'█';
+    int row = 0, col = 0;
+    while ((symbol = fgetwc(in)) != WEOF)
+    {
+        if (symbol == '\n')
+        {
+            row++;
+            col = 0;
+            continue;
+        }
+        if (row % 2 == 1)
+        {
+            if(symbol == black_wall)
+                board_structure[row/2][col/2] = BLACK_COLOR;
+            else if(symbol == white_wall)
+                board_structure[row/2][col/2] = WHITE_COLOR;
+        }
+        col++;
+    }
+    fclose(in);
+    return board_structure;
+}
